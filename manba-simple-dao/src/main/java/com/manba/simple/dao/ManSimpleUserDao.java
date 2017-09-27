60f2be214b2642c17cb8c23d3e0b7df882b7c3be
lijin <lijin@imdada.cn>
2017-09-27 19:12:18 +0800

    框架搭建-Spring Boot

--- manba-simple-dao/src/main/java/com/manba/simple/dao/ManSimpleUserDao.java
+++ manba-simple-dao/src/main/java/com/manba/simple/dao/ManSimpleUserDao.java
@@ -1,7 +1,5 @@
 package com.manba.simple.dao;
-import com.jd.o2o.road.common.dao.BaseDao;
-import com.manba.simple.domain.entity.ManSimpleUserEntity;
 
-public interface ManSimpleUserDao extends BaseDao<ManSimpleUserEntity, Long> {
+public interface ManSimpleUserDao {
 	
 }
