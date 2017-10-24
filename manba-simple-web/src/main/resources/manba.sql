24f867c28604d462130304d92818e6947c2bb753
lijin <lijin@imdada.cn>
2017-10-24 14:57:29 +0800

    sql文件

--- manba-simple-web/src/main/resources/manba.sql
+++ manba-simple-web/src/main/resources/manba.sql
@@ -1,26 +1,70 @@
 /*
 Navicat MySQL Data Transfer
-
-Source Server         : o2o-promotion-125
-Source Server Version : 50166
-Source Host           : 192.168.156.125:3306
 Source Database       : manba
-
-Target Server Type    : MYSQL
-Target Server Version : 50166
-File Encoding         : 65001
-
-Date: 2017-09-29 16:47:31
+Date: 2017-10-24 14:54:20
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for man_simple_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `man_simple_comment`;
+CREATE TABLE `man_simple_comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `zone_id` bigint(20) NOT NULL,
+  `content` varchar(2048) NOT NULL,
+  `comment_time` datetime DEFAULT NULL,
+  `parent_id` bigint(20) DEFAULT NULL,
+  `yn` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of man_simple_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for man_simple_favorite
+-- ----------------------------
+DROP TABLE IF EXISTS `man_simple_favorite`;
+CREATE TABLE `man_simple_favorite` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `zone_id` bigint(20) NOT NULL,
+  `favorite_time` datetime DEFAULT NULL,
+  `yn` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of man_simple_favorite
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for man_simple_follow
+-- ----------------------------
+DROP TABLE IF EXISTS `man_simple_follow`;
+CREATE TABLE `man_simple_follow` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `follow_id` bigint(20) NOT NULL,
+  `follow_time` datetime DEFAULT NULL,
+  `yn` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of man_simple_follow
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for man_simple_guild
 -- ----------------------------
 DROP TABLE IF EXISTS `man_simple_guild`;
 CREATE TABLE `man_simple_guild` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `guild_name` varchar(255) NOT NULL,
   `member_num` int(11) DEFAULT NULL,
   `liveness` int(11) DEFAULT NULL,
@@ -38,11 +82,46 @@ CREATE TABLE `man_simple_guild` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for man_simple_photo
+-- ----------------------------
+DROP TABLE IF EXISTS `man_simple_photo`;
+CREATE TABLE `man_simple_photo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `photo_path` varchar(1024) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `busi_type` int(11) DEFAULT NULL,
+  `yn` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of man_simple_photo
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for man_simple_upvote
+-- ----------------------------
+DROP TABLE IF EXISTS `man_simple_upvote`;
+CREATE TABLE `man_simple_upvote` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `zone_id` bigint(20) NOT NULL,
+  `upvote_time` datetime DEFAULT NULL,
+  `yn` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of man_simple_upvote
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for man_simple_user
 -- ----------------------------
 DROP TABLE IF EXISTS `man_simple_user`;
 CREATE TABLE `man_simple_user` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `phone` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nick_name` varchar(255) DEFAULT NULL,
@@ -52,7 +131,7 @@ CREATE TABLE `man_simple_user` (
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of man_simple_user
@@ -64,7 +143,7 @@ INSERT INTO `man_simple_user` VALUES ('1', '18516145120', '123456', '张三', '1
 -- ----------------------------
 DROP TABLE IF EXISTS `man_simple_zone`;
 CREATE TABLE `man_simple_zone` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `zone_title` varchar(1024) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   `zone_content` varchar(2048) DEFAULT NULL,
@@ -73,13 +152,16 @@ CREATE TABLE `man_simple_zone` (
   `yn` int(2) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of man_simple_zone
 -- ----------------------------
-INSERT INTO `man_simple_zone` VALUES ('1', '一个十年门户老编的哀叹：被今日头条革了命', '1', '一个十年门户老编的哀叹：被今日头条革了命', 'www.baidu.com/1.jpg', '2017-09-29 14:34:48', null, null);
-INSERT INTO `man_simple_zone` VALUES ('2', '被赔“葱”的宾利车主现身 称赔“葱”大爷有点儿意思', '2', '被赔“葱”的宾利车主现身 称赔“葱”大爷有点儿意思', 'www.baidu.com/2.jpg', '2017-09-29 14:35:29', null, null);
-INSERT INTO `man_simple_zone` VALUES ('3', '皇马续约阿森西奥至2023 年薪350万欧解约金5亿', '3', '皇马续约阿森西奥至2023 年薪350万欧解约金5亿', 'www.baidu.com/3.jpg', '2017-09-29 14:35:58', null, null);
-INSERT INTO `man_simple_zone` VALUES ('4', '英国发现罗马时期铜制小狗雕像 伸舌头表情生动', '4', '英国发现罗马时期铜制小狗雕像 伸舌头表情生动', 'www.baidu.com/4.jpg', '2017-09-29 14:36:37', null, null);
-INSERT INTO `man_simple_zone` VALUES ('5', '中国全面“封杀”比特币 外媒：戳破国际市场泡沫', '5', '中国全面“封杀”比特币 外媒：戳破国际市场泡沫', 'www.baidu.com/5.jpg', '2017-09-29 14:37:05', null, null);
+INSERT INTO `man_simple_zone` VALUES ('1', '一个十年门户老编的哀叹：被今日头条革了命', '1', '一个十年门户老编的哀叹：被今日头条革了命', 'www.baidu.com/1.jpg', '2017-09-29 14:34:48', '0', null);
+INSERT INTO `man_simple_zone` VALUES ('2', '被赔“葱”的宾利车主现身 称赔“葱”大爷有点儿意思', '2', '被赔“葱”的宾利车主现身 称赔“葱”大爷有点儿意思', 'www.baidu.com/2.jpg', '2017-09-29 14:35:29', '0', null);
+INSERT INTO `man_simple_zone` VALUES ('3', '皇马续约阿森西奥至2023 年薪350万欧解约金5亿', '3', '皇马续约阿森西奥至2023 年薪350万欧解约金5亿', 'www.baidu.com/3.jpg', '2017-09-29 14:35:58', '0', null);
+INSERT INTO `man_simple_zone` VALUES ('4', '英国发现罗马时期铜制小狗雕像 伸舌头表情生动', '4', '英国发现罗马时期铜制小狗雕像 伸舌头表情生动', 'www.baidu.com/4.jpg', '2017-09-29 14:36:37', '0', null);
+INSERT INTO `man_simple_zone` VALUES ('5', '中国全面“封杀”比特币 外媒：戳破国际市场泡沫', '5', '中国全面“封杀”比特币 外媒：戳破国际市场泡沫', 'www.baidu.com/5.jpg', '2017-09-29 14:37:05', '0', null);
+INSERT INTO `man_simple_zone` VALUES ('6', '测试', '2', '测试内容', 'www.baidu.com/dd.jpg', '2017-09-29 16:48:46', '1', '2017-09-29 17:11:50');
+INSERT INTO `man_simple_zone` VALUES ('7', '测试1', '2', '测试内容1', 'www.baidu.com/dd1.jpg', '2017-09-29 16:50:17', '0', null);
+INSERT INTO `man_simple_zone` VALUES ('8', '测试3', '3', '测试内容3', 'www.baidu.com/dd3.jpg', '2017-09-29 17:07:41', '0', null);
