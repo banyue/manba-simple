2a7d7d3ec74cd046e30dcedd45e16e29610f00cc
lijin <lijin@imdada.cn>
2017-10-27 14:02:56 +0800

    单元测试

---
+++ manba-simple-web/src/test/java/com/manba/test/ZoneServiceTest.java
@@ -0,0 +1,32 @@
+package com.manba.test;
+
+import com.manba.simple.ManBaApplicationLauncher;
+import com.manba.simple.api.OpenUserService;
+import com.manba.simple.domain.request.UserLoginRequest;
+import com.manba.simple.domain.response.ServiceResponse;
+import com.manba.simple.domain.response.UserInfoResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by lijin on 2017/10/26.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ManBaApplicationLauncher.class)
+public class ZoneServiceTest {
+
+    @Autowired
+    OpenUserService openUserService;
+
+    @Test
+    public void testOneUser() {
+        UserLoginRequest request = new UserLoginRequest();
+        request.setUserId("1");
+        ServiceResponse<UserInfoResponse> serviceResponse = openUserService.queryUserInfo(request);
+        UserInfoResponse response = serviceResponse.getResult();
+        System.out.println(response.getPhone());
+    }
+}
